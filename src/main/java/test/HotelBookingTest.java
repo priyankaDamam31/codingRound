ac5fb0fa2ba738f39924daa9cd6070b63d29e522
qaadmin <qaadmin@ny-vpc-qa5.na.rtdom.net>
2019-07-25 09:01:09 -0400

    Removed hard waits from the HotelBookingTest

--- src/main/java/test/HotelBookingTest.java
+++ src/main/java/test/HotelBookingTest.java
@@ -1,44 +1,26 @@
 package test;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
-import pagefactory.HotelBooking;
-import utils.ObjectRepositroy;
-import utils.TestHandler;
+import pagefactory.HotelBookingPage;
+import utils.ConstantRepositroy;
 
-public class HotelBookingTest extends TestHandler {
+public class HotelBookingTest {
 
 	WebDriver driver;
+	HotelBookingPage page = new HotelBookingPage(this.driver);
 
 	@Test
 	public void shouldBeAbleToSearchForHotels() {
 
-		// get the driver object and Navigate to URL
-		driver = getDriver();
-		driver.get(ObjectRepositroy.URL);
-
-		// initialize objects of HotelBooking page
-		HotelBooking page = PageFactory.initElements(driver, HotelBooking.class);
-
 		// Navigate to Hotels tab and search for hotels
 		page.clickHotelsLink();
-		page.sendKeysToLocality("Indiranagar, Bangalore");
-		elementHandler.waitForListOfElements(ObjectRepositroy.LOCALITY_OPTIONS);
-		List<WebElement> LocalityOptions = driver.findElement(By.id(ObjectRepositroy.LOCALITY_OPTIONS_ID))
-				.findElements(By.tagName("a"));
-		LocalityOptions.get(0).click();
-		elementHandler.waitFor(500);
+		page.selectLocality(ConstantRepositroy.LOCATION);
 		page.selectTravellers("1 room, 2 adults");
-		elementHandler.waitFor(500);
 
 		// Click on search button
 		page.clickSearch();
-		elementHandler.waitFor(3000);
+		page.logoutApplication();
 	}
 }
