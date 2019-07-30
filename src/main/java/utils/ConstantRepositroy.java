398160d00222296d676ea420b47fe151c77d148e
qaadmin <qaadmin@ny-vpc-qa5.na.rtdom.net>
2019-07-30 02:20:31 -0400

    Removed ObjectRepository.java file which used to contain Webelement locators and added ConstantRepository.java file which only contains the constants

---
+++ src/main/java/utils/ConstantRepositroy.java
@@ -0,0 +1,22 @@
+package utils;
+
+public final class ConstantRepositroy {
+
+	// Define all the constants in this class
+	/**
+	 * Value = {@value}
+	 */
+	public static final String URL = "https://www.cleartrip.com/";
+
+	public static final String ERRORS_TXT = "There were errors in your submission";
+	
+	public static final String TRIP_TYPE = "oneway";
+	
+	public static final String FROM_LOCATION = "Bangalore";
+	
+	public static final String TO_LOCATION = "Delhi";
+	
+	public static final String LOCATION = "Indiranagar, Bangalore";
+	
+	public static final String TRAVELLER_INFO = "1 room, 2 adults";
+}
