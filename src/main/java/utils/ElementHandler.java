6de54e5249debb7b7b1feea71bc26d8a3b126065
qaadmin <qaadmin@ny-vpc-qa5.na.rtdom.net>
2019-07-30 02:17:59 -0400

    Changes to ElementHandler.java 1. Added waitForElement method with explicit wait 2. Added waitForElementToBeClickable method with explicit wait 3. Added sendKeysToElement method 4. Added pressEscapeKey method

--- src/main/java/utils/ElementHandler.java
+++ src/main/java/utils/ElementHandler.java
@@ -1,9 +1,11 @@
 package utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,30 +17,65 @@ public class ElementHandler {
 		this.driver = driver;
 	}
 
-	public void waitFor(int durationInMilliSeconds) {
+	public void waitForElement(WebElement ele) {
 		try {
-			Thread.sleep(durationInMilliSeconds);
-		} catch (InterruptedException e) {
-			e.printStackTrace(); 
+			WebDriverWait wait = new WebDriverWait(driver, 30);
+			wait.until(ExpectedConditions.visibilityOf(ele));
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void waitForElementToBeClickable (WebElement ele) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 30);
+			wait.until(ExpectedConditions.elementToBeClickable(ele));
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
 		}
 	}
 
-	public boolean isElementPresent(By by) {
+	public boolean isElementPresent(WebElement ele) {
 		try {
-			driver.findElement(by);
+			ele.isDisplayed();
 			return true;
 		} catch (NoSuchElementException e) {
 			return false;
 		}
 	}
 
-	public void waitForListOfElements(String value) {
-		WebDriverWait wait = new WebDriverWait(driver, 60);
-		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath(value), 0));
+	public void waitForListOfElements(By value) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 60);
+			wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(value, 0));
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void sendKeysToElement(WebElement ele, String value) {
-		ele.clear();
-		ele.sendKeys(value);
+		try {
+			ele.clear();
+			ele.sendKeys(value);
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void navigateToFrame(WebElement frame) {
+		try {
+			driver.switchTo().frame(frame);
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void pressEscapeKey () {
+		try {
+			Actions act = new Actions(driver);
+			act.sendKeys(Keys.ESCAPE).perform();
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
 	}
 }
